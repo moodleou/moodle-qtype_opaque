eb8df52119717c899a147f66c4abad07b8ec3e6a
Tim Hunt <T.J.Hunt@open.ac.uk>
2011-08-26 16:15:56 +0100

    Refactor opaque code into separate files. #19

---
+++ enginemanager.php
@@ -0,0 +1,195 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Defines the qtype_opaque_engine_manager class.
+ *
+ * @package    qtype
+ * @subpackage opaque
+ * @copyright  2006 The Open University
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+defined('MOODLE_INTERNAL') || die();
+
+
+/**
+ * Manages loading and saving question engine definitions to and from the database.
+ *
+ * @copyright  2010 The Open University
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class qtype_opaque_engine_manager {
+    /**
+     * Load the definition of an engine from the database.
+     * @param int $engineid the id of the engine to load.
+     * @return mixed On success, and object with fields id, name, questionengines
+     *      and questionbanks. The last two fields are arrays of URLs. On an error,
+     *      returns a string to look up in the qtype_opaque language file as an
+     *      error message.
+     */
+    public function load_engine_def($engineid) {
+        global $DB;
+        $engine = $DB->get_record('question_opaque_engines',
+        array('id' => $engineid), '*', MUST_EXIST);
+
+        $engine->questionengines = array();
+        $engine->questionbanks = array();
+        $servers = $DB->get_records('question_opaque_servers',
+        array('engineid' => $engineid), 'id ASC');
+        if (!$servers) {
+            throw new moodle_exception('couldnotloadengineservers', 'qtype_opaque', '', $engineid);
+        }
+        foreach ($servers as $server) {
+            if ($server->type == 'qe') {
+                $engine->questionengines[] = $server->url;
+            } else if ($server->type == 'qb') {
+                $engine->questionbanks[] = $server->url;
+            } else {
+                throw new moodle_exception('unrecognisedservertype', 'qtype_opaque', '', $engineid);
+            }
+        }
+        return $engine;
+    }
+
+    /**
+     * Save or update an engine definition in the database, and returm the engine id. The definition
+     * will be created if $engine->id is not set, and updated if it is.
+     *
+     * @param object $engine the definition to save.
+     * @return int the id of the saved definition.
+     */
+    public function save_engine_def($engine) {
+        global $DB;
+        $transaction = $DB->start_delegated_transaction();
+
+        if (!empty($engine->id)) {
+            $DB->update_record('question_opaque_engines', $engine);
+        } else {
+            $engine->id = $DB->insert_record('question_opaque_engines', $engine);
+        }
+        $DB->delete_records('question_opaque_servers', array('engineid' => $engine->id));
+        $this->store_opaque_servers($engine->questionengines, 'qe', $engine->id);
+        $this->store_opaque_servers($engine->questionbanks, 'qb', $engine->id);
+
+        $transaction->allow_commit();
+        return $engine->id;
+    }
+
+    /**
+     * Save a list of servers of a given type in the question_opaque_servers table.
+     *
+     * @param array $urls an array of URLs.
+     * @param string $type 'qe' or 'qb'.
+     * @param int $engineid
+     */
+    protected function store_opaque_servers($urls, $type, $engineid) {
+        global $DB;
+        foreach ($urls as $url) {
+            $server = new stdClass();
+            $server->engineid = $engineid;
+            $server->type = $type;
+            $server->url = $url;
+            $DB->insert_record('question_opaque_servers', $server, false);
+        }
+    }
+
+    /**
+     * Delete the definition of an engine from the database.
+     * @param int $engineid the id of the engine to delete.
+     * @return bool whether the delete succeeded.
+     */
+    public function delete_engine_def($engineid) {
+        global $DB;
+        $transaction = $DB->start_delegated_transaction();
+        $DB->delete_records('question_opaque_servers', array('engineid' => $engineid));
+        $DB->delete_records('question_opaque_engines', array('id' => $engineid));
+        $transaction->allow_commit();
+    }
+
+    protected function get_possibly_matching_engines($engine) {
+        global $DB;
+
+        // First we try to get a reasonably accurate guess with SQL - we load
+        // the id of all engines with the same passkey and which use the first
+        // questionengine and questionbank (if any).
+        $tables = array('FROM {question_opaque_engines} e');
+        $conditions = array('e.passkey = :passkey');
+        $params = array('passkey' => $engine->passkey);
+        if (!empty($engine->questionengines)) {
+            $qeurl = reset($engine->questionengines);
+            $tables[] = "JOIN {question_opaque_servers} qe ON
+                    qe.engineid = e.id AND qe.type = 'qe'";
+            $conditions[] = 'qe.url = :qeurl';
+            $params['qeurl'] = $qeurl;
+        }
+        if (!empty($engine->questionbanks)) {
+            $qburl = reset($engine->questionbanks);
+            $tables[] = "JOIN {question_opaque_servers} qb ON
+                    qb.engineid = e.id AND qb.type = 'qb'";
+            $conditions[] = 'qb.url = :qburl';
+            $params['qburl'] = $qburl;
+        }
+        return $DB->get_records_sql_menu('
+                SELECT e.id,1 ' . implode(' ', $tables) . ' WHERE ' .
+        implode(' AND ', $conditions), $params);
+    }
+
+    /**
+     * If an engine definition like this one (same passkey and server lists) already exists
+     * in the database, then return its id, otherwise save this one to the database and
+     * return the new engine id.
+     *
+     * @param object $engine the engine to ensure is in the databse.
+     * @return int its id.
+     */
+    public function find_or_create_engineid($engine) {
+        $possibleengineids = $this->get_possibly_matching_engines($engine);
+
+        // Then we loop through the possibilities loading the full definition and comparing it.
+        if ($possibleengineids) {
+            foreach ($possibleengineids as $engineid => $ignored) {
+                $testengine = $this->load_engine_def($engineid);
+                $testengine->passkey = $testengine->passkey;
+                if ($this->is_same_engine($engine, $testengine)) {
+                    return $engineid;
+                }
+            }
+        }
+
+        return $this->save_engine_def($engine);
+    }
+
+    /**
+     * Are these two engine definitions essentially the same (same passkey and server lists)?
+     *
+     * @param object $engine1 one engine definition.
+     * @param object $engine2 another engine definition.
+     * @return bool whether they are the same.
+     */
+    public function is_same_engine($engine1, $engine2) {
+        // Same passkey.
+        $ans = $engine1->passkey == $engine2->passkey &&
+        // Same question engines.
+        !array_diff($engine1->questionengines, $engine2->questionengines) &&
+        !array_diff($engine2->questionengines, $engine1->questionengines) &&
+        // Same question banks.
+        !array_diff($engine1->questionbanks, $engine2->questionbanks) &&
+        !array_diff($engine2->questionbanks, $engine1->questionbanks);
+        return $ans;
+    }
+}
---
+++ resourcecache.php
@@ -0,0 +1,211 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Defines the qtype_opaque_resource_cache class.
+ *
+ * @package    qtype
+ * @subpackage opaque
+ * @copyright  2006 The Open University
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+defined('MOODLE_INTERNAL') || die();
+
+
+/**
+ * This class caches the resources belonging a particular question.
+ *
+ * There are synchronisation issues if two students are doing the same question
+ * at the same time.
+ *
+ * @copyright  2010 The Open University
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class qtype_opaque_resource_cache {
+    protected $folder; // Path to the folder where resources for this question are cached.
+    protected $metadatafolder; // Path to the folder where mime types are stored.
+    protected $baseurl; // initial part of the URL to link to a file in the cache.
+
+    /**
+     * Create a new qtype_opaque_resource_cache for a particular remote question.
+     * @param int $engineid the id of the question engine.
+     * @param string $remoteid remote question id, as per Opaque spec.
+     * @param string $remoteversion remote question version, as per Opaque spec.
+     */
+    public function __construct($engineid, $remoteid, $remoteversion) {
+        global $CFG;
+        $folderstart = $CFG->dataroot . '/opaqueresources/' . $engineid . '/' .
+        $remoteid . '/' . $remoteversion;
+        $this->folder = $folderstart . '/files';
+        if (!is_dir($this->folder)) {
+            $this->mkdir_recursive($this->folder);
+        }
+        $this->metadatafolder = $folderstart . '/meta';
+        if (!is_dir($this->metadatafolder)) {
+            $this->mkdir_recursive($this->metadatafolder);
+        }
+        $this->baseurl = "/question/type/opaque/file.php/{$engineid}/{$remoteid}/{$remoteversion}/";
+    }
+
+    /**
+     * @param string $filename the file name.
+     * @return the full path of a file with the given name.
+     */
+    public function file_path($filename) {
+        return $this->folder . '/' . $filename;
+    }
+
+    /**
+     * @param string $filename the file name.
+     * @return the full path of a file with the given name.
+     */
+    public function file_meta_path($filename) {
+        return $this->metadatafolder . '/' . $filename;
+    }
+
+    /**
+     * @param string $filename the file name.
+     * @return the URL to access this file.
+     */
+    public function file_url($filename) {
+        return new moodle_url($this->baseurl . $filename);
+    }
+
+    /**
+     * @param string $filename the file name.
+     * @return the URL to access this file.
+     */
+    public function file_mime_type($filename) {
+        $metapath = $this->file_meta_path($filename);
+        if (file_exists($metapath)) {
+            return file_get_contents($metapath);
+        }
+        return mimeinfo('type', $filename);
+    }
+
+    /**
+     * @param string $filename the name of the file to look for.
+     * @return true if this named file is in the cache, otherwise false.
+     */
+    public function file_in_cache($filename) {
+        return file_exists($this->file_path($filename));
+    }
+
+    /**
+     * Serve a file from the cache.
+     * @param string $filename the file name.
+     */
+    public function serve_file($filename) {
+        if (!$this->file_in_cache($filename)) {
+            header('HTTP/1.0 404 Not Found');
+            header('Content-Type: text/plain;charset=UTF-8');
+            echo 'File not found';
+            exit;
+        }
+        $mimetype = $this->file_mime_type($filename);
+
+        // Handle If-Modified-Since
+        $file = $this->file_path($filename);
+        $filedate = filemtime($file);
+        $ifmodifiedsince = isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) ?
+        $_SERVER['HTTP_IF_MODIFIED_SINCE'] : false;
+        if ($ifmodifiedsince && strtotime($ifmodifiedsince) >= $filedate) {
+            header('HTTP/1.0 304 Not Modified');
+            exit;
+        }
+        header('Last-Modified: '.gmdate('D, d M Y H:i:s', $filedate).' GMT');
+
+        // Type
+        header('Content-Type: ' . $mimetype);
+        header('Content-Length: ' . filesize($file));
+
+        // Output file
+        session_write_close(); // unlock session during fileserving
+        readfile($file);
+    }
+
+    /**
+     * Store a file in the cache.
+     *
+     * @param string $filename the name of the file to cache.
+     * @param string $mimetype the type of the file to cache.
+     * @param string $content the contents to write to the file.
+     */
+    public function cache_file($filename, $mimetype, $content) {
+        file_put_contents($this->file_path($filename), $content);
+        file_put_contents($this->file_meta_path($filename), $mimetype);
+    }
+
+    /**
+     * Add the resources from a particular response to the cache.
+     * @param array $resources as returned from start or process Opaque methods.
+     */
+    public function cache_resources($resources) {
+        if (!empty($resources)) {
+            foreach ($resources as $resource) {
+                $mimetype = $resource->mimeType;
+                if (strpos($resource->mimeType, 'text/') === 0 && !empty($resource->encoding)) {
+                    $mimetype .= ';charset=' . $resource->encoding;
+                }
+                $this->cache_file($resource->filename, $mimetype, $resource->content);
+            }
+        }
+    }
+
+    /**
+     * List the resources cached for this question.
+     * @return array list of resource names.
+     */
+    public function list_cached_resources() {
+        $filepaths = glob($this->folder . '/*');
+        if (!is_array($filepaths)) {
+            // If an error occurrs, say that we have no files cached.
+            $filepaths = array();
+        }
+        $pathlen = strlen($this->folder . '/');
+        $files = array();
+        foreach ($filepaths as &$filepath) {
+            $file = substr($filepath, $pathlen);
+            if (strpos($file, OPAQUE_CSS_FILENAME_PREFIX) !== 0) {
+                $files[] = $file;
+            }
+        }
+        return $files;
+    }
+
+    /**
+     * This function exists because mkdir(folder,mode,TRUE) doesn't work on our server.
+     * Safe to call even if folder already exists (checks)
+     * @param string $folder Folder to create
+     * @param int $mode Mode for creation (default 0755)
+     * @return bool True if folder (now) exists, false if there was a failure
+     */
+    protected function mkdir_recursive($folder, $mode='') {
+        if (is_dir($folder)) {
+            return true;
+        }
+        if ($mode == '') {
+            global $CFG;
+            $mode = $CFG->directorypermissions;
+        }
+        if (!$this->mkdir_recursive(dirname($folder), $mode)) {
+            return false;
+        }
+        return mkdir($folder, $mode);
+    }
+}
